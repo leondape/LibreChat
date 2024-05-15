4d777a575f6a665fc0bb07ffd4f7d08020b5b24d
Danilo Pejakovic <danilo.pejakovic@leoninestudios.com>
2024-05-15 13:50:41 +0000

    splitted cron and autoreset logic

---
+++ config/cron.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const cron = require('node-cron');
+const { resetBalancesCallback } = require('./reset-balance-auto.js'); // Update the path accordingly
+
+const { RESET_BALANCE, RESET_BALANCE_TIME } = process.env;
+
+// Check if RESET_BALANCE is true
+if (RESET_BALANCE === 'true') {
+  // Schedule the cron job
+  const task = cron.schedule(RESET_BALANCE_TIME, () => {
+    resetBalancesCallback();
+  });
+
+  // Log when the cron job is scheduled
+  task.start();
+  console.log(`Cron job scheduled at ${RESET_BALANCE_TIME}`);
+} else {
+  console.log('Balance reset is disabled.');
+}
